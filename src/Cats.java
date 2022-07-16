f4cddb88dd28062c63331ab725dc9bd41ea44960
Rinat Uzanzaev <wilax175@gmail.com>
2022-07-16 12:50:13 +0600

    changing the output of toString

--- src/Cats.java
+++ src/Cats.java
@@ -55,12 +55,8 @@ public class Cats {
 
     @Override
     public String toString() {
-        return "Cats{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                ", satiety=" + satiety +
-                ", mood=" + mood +
-                ", health=" + health +
-                '}';
+        int average = (health + mood + satiety) / 3;
+        return String.format(" | %8s | %4d | %7d | %4d | %6d | %7d |",
+                name, age, satiety, mood, health, average);
     }
 }
