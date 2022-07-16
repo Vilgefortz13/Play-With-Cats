8a267cc74ee4bf4f4976a14bea5fcac93bd9487f
Rinat Uzanzaev <wilax175@gmail.com>
2022-07-16 12:16:48 +0600

    test array filling

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        run();
+    }
+
+    private static void run() {
+        List<Cats> cats = Arrays.asList(
+                new Cats(
+                        "Peach",
+                        7,
+                        (int) (Math.random() * 60) + 20,
+                        (int) (Math.random() * 60) + 20,
+                        (int) (Math.random() * 60) + 20),
+                new Cats(
+                        "Jasper",
+                        3,
+                        (int) (Math.random() * 60) + 20,
+                        (int) (Math.random() * 60) + 20,
+                        (int) (Math.random() * 60) + 20),
+                new Cats(
+                        "Poppy",
+                        10,
+                        (int) (Math.random() * 60) + 20,
+                        (int) (Math.random() * 60) + 20,
+                        (int) (Math.random() * 60) + 20)
+        );
+
+
+        System.out.println(cats);
+
     }
 }
