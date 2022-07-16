6e0f5bc0c639c46bc5f2f3da7410267161cba42d
Rinat Uzanzaev <wilax175@gmail.com>
2022-07-16 11:51:00 +0600

    added getters and override toString

--- src/Cats.java
+++ src/Cats.java
@@ -4,4 +4,35 @@ public class Cats {
     private Integer satiety;
     private Integer mood;
     private Integer health;
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public Integer getSatiety() {
+        return satiety;
+    }
+
+    public Integer getMood() {
+        return mood;
+    }
+
+    public Integer getHealth() {
+        return health;
+    }
+
+    @Override
+    public String toString() {
+        return "Cats{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", satiety=" + satiety +
+                ", mood=" + mood +
+                ", health=" + health +
+                '}';
+    }
 }
