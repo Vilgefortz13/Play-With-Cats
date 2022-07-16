9d5d35c5f2b6b8953795fdfe724d4f3b95902a18
Rinat Uzanzaev <wilax175@gmail.com>
2022-07-16 17:41:51 +0600

    added event 'to next day'

--- src/Main.java
+++ src/Main.java
@@ -19,39 +19,46 @@ public class Main {
                     getRandomNumber()),
             new Cats(
                     "Poppy",
-                    10,
+                    12,
                     getRandomNumber(),
                     getRandomNumber(),
                     getRandomNumber())
     );
+
     public static void main(String[] args) {
         run();
     }
 
     private static void run() {
-        cats = cats.stream()
-                .sorted(Collections.reverseOrder(Comparator.comparing(Cats::getAverage)))
-                .collect(toList());
-
         printCats(cats);
-        System.out.print("""
-                \nEnter the action:
-                  add the cat - 1;
-                  feed - 2;
-                  play - 3;
-                  take to the vet - 4;
-                >\040""");
-        int action = getNumber(1, 4);
+        while (true) {
+            cats = cats.stream()
+                    .sorted(Collections.reverseOrder(Comparator.comparing(Cats::getAverage)))
+                    .collect(toList());
 
-        doAction(cats, action);
-        printCats(cats);
+            System.out.print("""
+                    \nEnter the action:
+                      add the cat - 1;
+                      feed - 2;
+                      play - 3;
+                      take to the vet - 4;
+                      next day - 5;
+                      complete the program - 6
+                    >\040""");
+            int action = getNumber(1, 6);
+            if (action == 6) {
+                break;
+            }
+            doAction(cats, action);
+            printCats(cats);
+        }
     }
 
     private static void doAction(List<Cats> cats, int action) {
         switch (action) {
             case 1 -> {
                 System.out.print("Enter cat's name: ");
-                String name = getString(new Scanner(System.in));
+                String name = firstUpperCase(getString(new Scanner(System.in)));
                 System.out.print("Enter cat's age (1 - 18): ");
                 int age = getNumber(1, 18);
                 cats.add(
@@ -64,23 +71,39 @@ public class Main {
             case 2 -> {
                 System.out.print("Enter cat's number: ");
                 int index = getNumber(1, cats.size()) - 1;
+                System.out.println("You fed " + cats.get(index).getName());
                 cats.get(index).setActivity(new Feeding());
                 cats.get(index).catsAction(index);
             }
             case 3 -> {
                 System.out.print("Enter cat's number: ");
                 int index = getNumber(1, cats.size()) - 1;
+                System.out.println("You played with " + cats.get(index).getName());
                 cats.get(index).setActivity(new Playing());
                 cats.get(index).catsAction(index);
             }
             case 4 -> {
                 System.out.print("Enter cat's number: ");
                 int index = getNumber(1, cats.size()) - 1;
+                System.out.println("You healed " + cats.get(index).getName());
                 cats.get(index).setActivity(new Healing());
                 cats.get(index).catsAction(index);
             }
+            case 5 -> {
+                Random rnd = new Random();
+                for (Cats cat : cats) {
+                    cat.setSatiety(cat.getSatiety() - rnd.nextInt(1,6));
+                    cat.setMood(cat.getMood() + rnd.nextInt(-3, 4));
+                    cat.setHealth(cat.getHealth() + rnd.nextInt(-3, 4));
+                }
+            }
             default -> System.out.println();
         }
+    }
+
+    public static String firstUpperCase(String word){
+        if(word == null || word.isEmpty()) return "";
+        return word.substring(0, 1).toUpperCase() + word.substring(1);
     }
 
     public static void printCats(List<Cats> cats) {
